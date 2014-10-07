523198bece1d44b643fd6bcc7963b5f50677f45f
Joshua Henderson <joshua.henderson@microchip.com>
2014-10-07 15:00:48 -0700

    net: phy: Add support for SMSC LAN8740 PHY.
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- drivers/net/phy/smsc.c
+++ drivers/net/phy/smsc.c
@@ -255,6 +255,28 @@ static struct phy_driver smsc_phy_driver[] = {
 	.resume		= genphy_resume,
 
 	.driver		= { .owner = THIS_MODULE, }
+}, {
+	.phy_id		= 0x0007c110,
+	.phy_id_mask	= 0xfffffff0,
+	.name		= "SMSC LAN8740",
+
+	.features	= (PHY_BASIC_FEATURES | SUPPORTED_Pause
+				| SUPPORTED_Asym_Pause),
+	.flags		= PHY_HAS_INTERRUPT | PHY_HAS_MAGICANEG,
+
+	/* basic functions */
+	.config_aneg	= genphy_config_aneg,
+	.read_status	= lan87xx_read_status,
+	.config_init	= smsc_phy_config_init,
+
+	/* IRQ related */
+	.ack_interrupt	= smsc_phy_ack_interrupt,
+	.config_intr	= smsc_phy_config_intr,
+
+	.suspend	= genphy_suspend,
+	.resume		= genphy_resume,
+
+	.driver		= { .owner = THIS_MODULE, }
 } };
 
 module_phy_driver(smsc_phy_driver);
@@ -269,6 +291,7 @@ static struct mdio_device_id __maybe_unused smsc_tbl[] = {
 	{ 0x0007c0c0, 0xfffffff0 },
 	{ 0x0007c0d0, 0xfffffff0 },
 	{ 0x0007c0f0, 0xfffffff0 },
+	{ 0x0007c110, 0xfffffff0 },
 	{ }
 };
 
