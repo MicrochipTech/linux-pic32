8cf49fcd67182eb776db1b8a0c2f9a2bf0046ce8
Joshua Henderson <joshua.henderson@microchip.com>
2014-12-09 17:31:08 -0700

    rng: pic32-rng: Add PIC32 PRNG and TRNG hardware driver.
    Add a pseudo and true random number generator hardware driver for PIC32.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>
    Signed-off-by: Purna Chandra Mandal <purna.mandal@microchip.com>

---
+++ drivers/char/hw_random/pic32-rng.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2014 Joshua Henderson <joshua.henderson@microchip.com>
+ *
+ * This file is licensed under  the terms of the GNU General Public
+ * License version 2. This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/err.h>
+#include <linux/io.h>
+#include <linux/hw_random.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/clk.h>
+#include <linux/clkdev.h>
+
+#define DRIVER_NAME "pic32-rng"
+
+#define RNGCON		0x04
+#define RNGCON_TRNGEN	BIT(8)
+#define RNGCON_PRNGEN	BIT(9)
+#define RNGCON_CONT	BIT(10)
+#define RNGCON_LOAD	BIT(12)
+#define RNGPOLY1	0x08
+#define RNGPOLY2	0x0C
+#define RNGNUMGEN1	0x10
+#define RNGNUMGEN2	0x14
+#define RNGSEED1	0x18
+#define RNGSEED2	0x1C
+#define RNGRCNT		0x20
+#define RCNT_MASK	0x7F
+
+struct pic32_rng {
+	void __iomem *base;
+	struct hwrng rng;
+	struct clk *clk;
+};
+
+static int pic32_rng_read(struct hwrng *rng, void *buf, size_t max,
+			   bool wait)
+{
+	struct pic32_rng *prng = container_of(rng, struct pic32_rng, rng);
+	u64 *data = buf;
+
+	*data = ((u64)readl(prng->base + RNGNUMGEN2) << 32) +
+		readl(prng->base + RNGNUMGEN1);
+	return 8;
+}
+
+static const struct of_device_id pic32_rng_of_match[] = {
+	{ .compatible	= "microchip,pic32-rng", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, pic32_rng_of_match);
+
+static int pic32_rng_probe(struct platform_device *pdev)
+{
+	struct pic32_rng *prng;
+	struct resource *res;
+	u32 v;
+	int ret;
+
+	prng = devm_kzalloc(&pdev->dev, sizeof(*prng), GFP_KERNEL);
+	if (!prng)
+		return -ENOMEM;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	prng->base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(prng->base))
+		return PTR_ERR(prng->base);
+
+	prng->clk = devm_clk_get(&pdev->dev, NULL);
+	if (IS_ERR(prng->clk))
+		return PTR_ERR(prng->clk);
+
+	clk_prepare_enable(prng->clk);
+
+	/* enable TRNG */
+	v = readl(prng->base + RNGCON);
+	v &= ~(RNGCON_TRNGEN|RNGCON_PRNGEN|0xff);
+	writel(v|RNGCON_TRNGEN, prng->base + RNGCON);
+
+	/* wait for valid seed */
+	usleep_range(100, 200);
+	v = readl(prng->base + RNGRCNT) & RCNT_MASK;
+	if (v < 0x2A)
+		dev_warn(&pdev->dev, "seed not generated.\n");
+
+	/* stop TRNG and load initial seed */
+	writel(v|RNGCON_LOAD, prng->base + RNGCON);
+
+	/* load initial polynomial: 42bit poly */
+	writel(0x00c00003, prng->base + RNGPOLY1);
+	writel(0x00000000, prng->base + RNGPOLY2);
+
+	/* start PRNG to generate 42bit random */
+	v |= 0x2A|RNGCON_CONT|RNGCON_PRNGEN;
+	writel(v, prng->base + RNGCON);
+
+	prng->rng.name = pdev->name;
+	prng->rng.read = pic32_rng_read;
+
+	ret = hwrng_register(&prng->rng);
+	if (ret)
+		goto err_register;
+
+	platform_set_drvdata(pdev, prng);
+
+	dev_info(&pdev->dev, "PIC32 Random Number Generator\n");
+
+	return 0;
+
+err_register:
+	return ret;
+}
+
+static int pic32_rng_remove(struct platform_device *pdev)
+{
+	struct pic32_rng *rng = platform_get_drvdata(pdev);
+
+	hwrng_unregister(&rng->rng);
+	writel(0, rng->base + RNGCON);
+	clk_disable_unprepare(rng->clk);
+	return 0;
+}
+
+static struct platform_driver pic32_rng_driver = {
+	.probe		= pic32_rng_probe,
+	.remove		= pic32_rng_remove,
+	.driver		= {
+		.name	= DRIVER_NAME,
+		.owner	= THIS_MODULE,
+		.of_match_table = of_match_ptr(pic32_rng_of_match),
+	},
+};
+
+module_platform_driver(pic32_rng_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Joshua Henderson <joshua.henderson@microchip.com>");
+MODULE_DESCRIPTION("PIC32 pseudo random number generator driver");
