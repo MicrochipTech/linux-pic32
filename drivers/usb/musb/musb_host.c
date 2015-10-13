6f35cb32571bee5707295e22eccd25886995ae1b
Cristian Birsan <cristian.birsan@microchip.com>
2015-10-13 15:39:46 +0300

    usb: musb: Fix DMA for host mode
    Fix done condition that advances the musb schedule. Without this patch the the
    msub_advance_schedule() is called immediately after receiving an endpoint rx
    interrupt without waiting for the dma transfer to complete. As a consequence
    when the dma complete interrupt arrives the in_qh member of hw_ep is already
    null an the musb_host_rx() exits on !urb error case.
    
    Signed-off-by: Cristian Birsan <cristian.birsan@microchip.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -1815,6 +1815,7 @@ void musb_host_rx(struct musb *musb, u8 epnum)
 	u16			rx_csr, val;
 	bool			iso_err = false;
 	bool			done = false;
+	int			ret;
 	u32			status;
 	struct dma_channel	*dma;
 	unsigned int sg_flags = SG_MITER_ATOMIC | SG_MITER_TO_SG;
@@ -1993,10 +1994,10 @@ void musb_host_rx(struct musb *musb, u8 epnum)
 				qh->offset,
 				urb->transfer_buffer_length);
 
-			done = musb_rx_dma_in_inventra_cppi41(c, hw_ep, qh,
+			ret = musb_rx_dma_in_inventra_cppi41(c, hw_ep, qh,
 							      urb, xfer_len,
 							      iso_err);
-			if (done)
+			if (ret)
 				goto finish;
 			else
 				dev_err(musb->controller, "error: rx_dma failed\n");
