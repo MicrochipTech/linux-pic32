a466a0cb021111357666288927db237170ecfb1a
Joshua Henderson <joshua.henderson@microchip.com>
2015-11-17 14:01:56 -0700

    MIPS: pic32: Update file copyright header
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>
    
    Conflicts:
    
    	arch/mips/Kbuild.platforms

--- arch/mips/pic32/pic32mzda/common.h
+++ arch/mips/pic32/pic32mzda/common.h
@@ -1,9 +1,15 @@
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
+ * Joshua Henderson <joshua.henderson@microchip.com>
+ * Copyright (C) 2015 Microchip Technology Inc.  All rights reserved.
  *
- * Copyright (C) 2014 Joshua Henderson <joshua.henderson@microchip.com>
+ *  This program is free software; you can distribute it and/or modify it
+ *  under the terms of the GNU General Public License (Version 2) as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
  */
 #ifndef PIC32MZDA_COMMON_H
 #define PIC32MZDA_COMMON_H
--- arch/mips/pic32/pic32mzda/early_pin.c
+++ arch/mips/pic32/pic32mzda/early_pin.c
@@ -1,11 +1,15 @@
 /*
- * Copyright (C) 2014 Microchip Technology Inc.  All rights reserved.
- * Author: Joshua Henderson <joshua.henderson@microchip.com>
+ * Joshua Henderson <joshua.henderson@microchip.com>
+ * Copyright (C) 2015 Microchip Technology Inc.  All rights reserved.
  *
- * This program is free software; you can redistribute	it and/or modify it
- * under  the terms of	the GNU General	 Public License as published by the
- * Free Software Foundation;  either version 2 of the  License, or (at your
- * option) any later version.
+ *  This program is free software; you can distribute it and/or modify it
+ *  under the terms of the GNU General Public License (Version 2) as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
  */
 #include <asm/io.h>
 
