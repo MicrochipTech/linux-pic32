601c4a8d533380160fe2a128cd8d8647c0e96be9
Joshua Henderson <joshua.henderson@microchip.com>
2015-11-17 14:03:27 -0700

    MIPS: pic32: ocmp: Update copyright header
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- arch/mips/pic32/common/ocmp.c
+++ arch/mips/pic32/common/ocmp.c
@@ -1,10 +1,17 @@
 /*
  * PIC32 General Purpose Output Compare Driver.
  *
- * Copyright (c) 2014, Microchip Technology Inc.
- *      Purna Chandra Mandal <purna.mandal@microchip.com>
+ * Purna Chandra Mandal <purna.mandal@microchip.com>
+ * Copyright (C) 2015 Microchip Technology Inc.  All rights reserved.
  *
- * Licensed under GPLv2.
+ *  This program is free software; you can distribute it and/or modify it
+ *  under the terms of the GNU General Public License (Version 2) as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
  */
 #include <linux/types.h>
 #include <linux/init.h>
